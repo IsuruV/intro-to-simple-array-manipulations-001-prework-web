f5f294b32037394eb5a6faa152698a9f5841b08e
Isuru Vidanapathirana <isuru260@gmail.com>
2016-06-24 04:08:47 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,51 @@
+def using_push(array,item)
+  array.push(item)
+
+end
+
+def using_unshift(array,item)
+  array.unshift(item)
+
+end
+
+def using_pop(array)
+  array.pop
+
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
+
+def using_concat(array,array2)
+  array.concat(array2)
+end
+
+def using_insert(array, element)
+  array.insert(4,element)
+end
+
+def using_uniq(array)
+  array.uniq
+end
+
+def using_flatten(array)
+  array.flatten
+end
+
+
+def using_delete(array, input)
+  array.delete(input)
+end
+
+def using_delete_at(array,input)
+  array.delete_at(input)
+end
